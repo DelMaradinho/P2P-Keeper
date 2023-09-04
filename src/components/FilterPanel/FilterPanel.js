6c3c5ac31b4094a6a69a2b5fe8b90c3a2817dc84
dzerayeah <dzera.nat.i@gmail.com>
2023-09-04 16:25:42 +0300

    add filter

---
+++ src/components/FilterPanel/FilterPanel.js
@@ -0,0 +1,56 @@
+import { TreeSelect } from "antd";
+import { useState } from "react";
+import { filterData } from "../../constants/constants";
+
+const FilterPanel = ({ onFilterChange }) => {
+  const [value, setValue] = useState();
+
+  const onChange = (newValue) => {
+    const formattedValue = newValue.reduce((acc, currentValue) => {
+      // Проверяем, является ли currentValue родительским элементом
+      const parent = filterData.find(
+        (category) => category.value === currentValue
+      );
+      if (parent) {
+        acc[currentValue] = parent.children.map((child) => child.value);
+      } else {
+        // Если это не родительский элемент, ищем родительскую категорию для текущего элемента
+        for (let category of filterData) {
+          if (category.children.some((child) => child.value === currentValue)) {
+            if (!acc[category.value]) {
+              acc[category.value] = [];
+            }
+            acc[category.value].push(currentValue);
+            break; // Если значение найдено, прекращаем поиск в этой категории
+          }
+        }
+      }
+      return acc;
+    }, {});
+
+    onFilterChange(formattedValue);
+    setValue(newValue);
+  };
+
+  return (
+    <TreeSelect
+      showSearch
+      style={{
+        width: "200px",
+      }}
+      value={value}
+      dropdownStyle={{
+        maxHeight: 400,
+        overflow: "auto",
+      }}
+      placeholder="Выбери фильтр"
+      allowClear
+      multiple
+      treeDefaultExpandAll
+      onChange={onChange}
+      treeData={filterData}
+      treeCheckable={true}
+    />
+  );
+};
+export default FilterPanel;
---
+++ src/constants/constants.js
@@ -0,0 +1,67 @@
+export const filterData = [
+  {
+    value: "currency",
+    title: "Валюта",
+    children: [
+      {
+        value: "btc",
+        title: "BTC",
+      },
+      {
+        value: "usdt",
+        title: "USDT",
+      },
+    ],
+  },
+  {
+    value: "platform",
+    title: "Платформа",
+    children: [
+      {
+        value: "binance",
+        title: "Binance",
+      },
+      {
+        value: "bybit",
+        title: "Bybit",
+      },
+    ],
+  },
+];
+
+export const tableData = [
+  {
+    key: 1,
+    currency: "BTC",
+    buy_price: 12345,
+    buy_amount: 100,
+    exchange: "",
+    sell_price: 12350,
+    sell_amount: 99,
+    spread: "",
+    net_profit: "",
+    date: "2022-03-21",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    description:
+      "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+  },
+  {
+    key: 2,
+    currency: "USDT",
+    buy_price: 12345,
+    buy_amount: 100,
+    exchange: "",
+    sell_price: 12350,
+    sell_amount: 99,
+    spread: "",
+    net_profit: "",
+    date: "2022-02-01",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    description:
+      "My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.",
+  },
+];
