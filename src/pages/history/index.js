fe34524578f74d70eae398de57bb61e8f89a6bdb
dmitriev.a@technoavia.ru <dmitriev.a@technoavia.ru>
2023-09-05 15:54:55 +0600

    fix: header

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,12 +1,18 @@
 import MainMenu from "../../components/MainMenu/MainMenu";
 import CustomTable from "../../components/CustomTable/CustomTable";
+import "./history.scss";
 
 function History() {
   return (
     <div className="App">
-      <h1>История сделок</h1>
-      <MainMenu />
-      <CustomTable />
+      <div className="left">
+        <MainMenu />
+      </div>
+      <div className="right">
+        {" "}
+        <h1>История сделок</h1>
+        <CustomTable />
+      </div>
     </div>
   );
 }
