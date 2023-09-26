f25cb6f71f91ec2a9f7dc8128d717e23044fd452
dmitriev.a@technoavia.ru <dmitriev.a@technoavia.ru>
2023-09-26 13:02:14 +0600

    minor changes

--- src/components/DropArea/DropArea.js
+++ src/components/DropArea/DropArea.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DropArea.scss";
 
-const dragEnter = (e) => {
-  e.preventDefault();
-  e.target.className += " hovered"; // добавляем класс 'hovered'
-};
-
-const dragLeave = (e) => {
-  e.target.className = "droparea__item"; // устанавливаем изначальный класс
-};
-
-const dragOver = (e) => {
-  e.preventDefault();
-};
-
-const drop = (e) => {
-  e.target.className = "droparea__item"; // устанавливаем изначальный класс
-  const operation = e.dataTransfer.getData("operation");
-  e.target.textContent = operation;
-};
-
 function DropArea() {
+  const [droppedValue, setDroppedValue] = useState(null);
+
+  const dragEnter = (e) => {
+    e.preventDefault();
+    e.target.className += " hovered"; // добавляем класс 'hovered'
+  };
+
+  const dragLeave = (e) => {
+    e.target.className = "droparea__item"; // устанавливаем изначальный класс
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const drop = (e) => {
+    e.target.className = "droparea__item";
+    const operation = e.dataTransfer.getData("operation");
+    setDroppedValue(operation);
+  };
   return (
     <div
       className="droparea__item"
@@ -28,7 +29,9 @@ function DropArea() {
       onDragOver={dragOver}
       onDragEnter={dragEnter}
       onDragLeave={dragLeave}
-    ></div>
+    >
+      {droppedValue}
+    </div>
   );
 }
 
