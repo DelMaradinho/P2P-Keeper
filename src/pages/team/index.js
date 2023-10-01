3b74bfef0c3b9fcb164189940dce961d065a37ec
dmitriev.a@technoavia.ru <dmitriev.a@technoavia.ru>
2023-10-01 17:58:18 +0600

    feat: team table

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,15 +1,38 @@
 import React from "react";
 import MainMenu from "../../components/MainMenu/MainMenu";
+import TeamTable from "../../components/TeamTable/TeamTable";
 // import "./settings.scss";
+
+const team = [
+  {
+    name: "Дэвид Ливси",
+    role: "Доктор",
+    email: "d.livesey@espanyola.com",
+    key: 1,
+  },
+  {
+    name: "Джим Хокинс",
+    role: "Юнга",
+    email: "j.hawkins@espanyola.com",
+    key: 2,
+  },
+  {
+    name: "Джон Трелони",
+    role: "Сквайр",
+    email: "j.treloni@espanyola.com",
+    key: 3,
+  },
+];
 
 function Team() {
   return (
     <div className="App">
       <div className="left">
-        <MainMenu selectedKey={["7"]} />
+        <MainMenu selectedKey={["5"]} />
       </div>
       <div className="right">
         <h1 className="right__header">Команда</h1>
+        <TeamTable tableData={team} />
       </div>
     </div>
   );
